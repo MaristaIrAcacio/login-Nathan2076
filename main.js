d2077872baa9967357343c4536f63932838cecf2
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2023-07-04 12:11:05 -0300

    End of tuesday class

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 // Input filling verification block
 
-function Verify()
+function verify()
 {
     let user = document.getElementById("loginEmail").value;
     let pswr = document.getElementById("loginSenha").value;
 
-    if (!user || !pswr)
-    {
+    if (!user || !pswr) {
         alert("Campos de preenchimento obrigatório. Favor preencher!");
+    } else {
+        window.location.href = "aula2.html";
     }
-    else
-    {
-        alert("Campos preenchidos com sucesso!");
+}
+
+/* FUNCTION TO CREATE THE USER LIST
+ *
+ * Creates the Array to store users
+ */
+
+var dadosLista = [];
+
+function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if (nomeUser) {
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
     }
 }
